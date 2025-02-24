5983d33dc35800cdc9459630f6dadb65b810863f
gvpalma <gvpalma@cougarnet.uh.edu>
2025-02-24 15:12:39 -0600

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -1,1 +1,15 @@
+/* 
+Name:Gabriela Palma
+File name: homework2.js
+Date Created: February 24
+*/
 
+  <script>
+    function goToThankYouPage() {
+      window.location.href = "homework1-thankyou.html";}
+  </script>
+  
+   //dynamic date js code
+const d = new Date();
+let text = d.toLocaleDateString();
+document.getElementById("today").innerHTML = text;
